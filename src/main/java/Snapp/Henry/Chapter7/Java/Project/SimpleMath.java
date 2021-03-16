8920c44312b6c10ee9232b3f29e2fd8fcee185af
unknown <hssnapp@mtu.edu>
2021-03-16 15:22:33 -0400

    initial commit

---
+++ src/main/java/Snapp/Henry/Chapter7/Java/Project/SimpleMath.java
@@ -0,0 +1,12 @@
+package Snapp.Henry.Chapter7.Java.Project;
+
+import static org.junit.Assert.*;
+
+public class SimpleMath {
+	
+public void test1() {
+	SimpleMath simpleMath1 = new SimpleMath();
+	assertEquals(0, simpleMath1.divide(0, 7), 0.0001);
+		}
+	}
+}
